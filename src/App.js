43b20d0393d0ba029afc99f693bcc43eb0832f2d
Jaborov <dzhaborov.b@gmail.com>
2023-10-10 23:35:37 +0500

    add hooks and fetch API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useEffect } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 import PostForm from "./components/PostForm";
@@ -6,18 +6,22 @@ import PostFiltr from "./components/PostFiltr";
 import MyModal from "./components/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/button/loading/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "zzzzz", body: "ttttt" },
-    { id: 2, title: "aaaaa", body: "ggggg" },
-    { id: 3, title: "sdds", body: "eeeee" },
-    { id: 4, title: "bbbb", body: "ppppp" },
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [modal, setModal] = useState(false);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -37,14 +41,26 @@ function App() {
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-
       <hr style={{ margin: "15px 0" }} />
       <PostFiltr filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={"Список постов 1"}
-      />
+      {postError && <h1>Error status ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title={"Список постов 1"}
+        />
+      )}
     </div>
   );
 }
